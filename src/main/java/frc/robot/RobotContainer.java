43eb037974573c8d1bbbcdda7834946841fb650c
Driver3467 <frc_3467_driver@team3467.org>
2026-03-31 21:10:51 -0400

    revamped max speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -119,6 +119,10 @@ public class RobotContainer {
         LoggedTuneableProfiledPID linearController =
             new LoggedTuneableProfiledPID("DriveToPose/LinearController", 3.0, 0, 0.1, 0, 3.0);
 
+        // nice values
+        // kp = 70
+        // ki = 0
+        // kd = 0
         LoggedTuneableProfiledPID drivePID =
             new LoggedTuneableProfiledPID("DrivePID", 60.0, 0, 0, 5.0, 0);
         SmartDashboard.putData("SubmitDrivePID", drive.setDrivePID(drivePID.convert()));
